0d436c258acd09903e4fb0648cc4fbc90e66cd80
saskiyya <saskiahumaira02@gmail.com>
2025-09-03 12:34:20 +0700

    Add files via upload

--- app_streamlit.py
+++ app_streamlit.py
@@ -1,17 +1,16 @@
 import streamlit as st
- import pandas as pd
- import joblib
- loaded_model = joblib.load("mlbb_model.joblib")
- st.title("Machine Learning Classification")
- st.markdown("Ini adalah aplikasi untuk memprediksi jenis role penyerang atau bertahan di MLBB")
- kill = st.slider("Jumlah Kill", 0, 20)
- assist = st.slider("Jumlah Assist", 0, 20)
- death = st.slider("Jumlah Death", 0, 20)
- turret = st.slider("Jumlah Turret", 0, 20)
- if st.button("Prediksi"):
- data_baru = pd.DataFrame([[kill,assist,death,turret]], 
-columns=["kill","assist","death","turret"])
+import pandas as pd
+import joblib
+loaded_model = joblib.load("mlbb_model.joblib")
+st.title("Machine Learning Classification")
+st.markdown("Ini adalah aplikasi untuk memprediksi jenis role penyerang atau bertahan di MLBB")
+kill = st.slider("Jumlah Kill", 0, 20)
+assist = st.slider("Jumlah Assist", 0, 20)
+death = st.slider("Jumlah Death", 0, 20)
+turret = st.slider("Jumlah Turret", 0, 20)
+if st.button("Prediksi"):
+    data_baru = pd.DataFrame([[kill,assist,death,turret]],columns=["kill","assist","death","turret"])
  
-st.success(f"Hasil Prediksi : {loaded_model.predict(data_baru)[0]}")
- st.balloons()
- st.caption("Dibuat dengan :heart: oleh saskia")
+    st.success(f"Hasil Prediksi : {loaded_model.predict(data_baru)[0]}")
+    st.balloons()
+st.caption("Dibuat dengan :heart: oleh saskia")
